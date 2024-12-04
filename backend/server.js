df94f531315e473159616c5f93cff6af700c0665
ShivamGupta322 <guptashivamsg02@gmail.com>
2024-12-04 17:40:26 +0530

    Therapist added to frontend through backend api

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudnary.js';
 import adminRouter from './routes/adminRoutes.js';
+import doctorRouter from './routes/doctorRoutes.js';
 
 
 
@@ -28,6 +29,7 @@ app.use(cors());
 
 app.use('/api/admin',adminRouter)
 //localhost:4000/api/admin/add-doctor
+app.use('/api/doctor',doctorRouter)
 
 app.get('/', (req, res) => {
   res.send('API Working');
