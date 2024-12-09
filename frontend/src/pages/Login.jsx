6ab600e490809266dbe602f3309c46e1a2ae996e
ShivamGupta322 <guptashivamsg02@gmail.com>
2024-12-09 21:29:34 +0530

    Payment Integration

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -350,7 +350,7 @@ const Login = () => {
     // }
 
     try{
-      if(state=='sign Up'){
+      if(state=='Sign Up'){
         const {data}= await axios.post(backendUrl + '/api/user/register',{name,password,email})
         if(data.success){
           localStorage.setItem('token',data.token)
