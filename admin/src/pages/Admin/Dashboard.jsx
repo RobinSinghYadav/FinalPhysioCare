174d17c06be03857e3593fd93813401663f8a588
Ansh681u <anshu.gangwar491@gmail.com>
2024-12-10 14:02:00 +0530

    Admin Panel

--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { AdminContext } from '../../context/AdminContext'
+import { assets } from '../../assets/assets'
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
+
+  const {aToken,getDashData,cancelAppointment,dashData}=useContext(AdminContext)
+
+  useEffect(()=>{
+    if(aToken){
+      getDashData()
+    }
+  },[aToken])
+  return dashData &&(
+    <div className='m-5'>
+      <div className='flex flex-wrap gap-3'>
+        <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
+          <img src={assets.doctor_icon} alt="" />
+          <div>
+            <p>{dashData.doctors}</p>
+            <p>Doctors</p>
+          </div>
+        </div>
+
+        <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
+          <img className='w-14' src={assets.appointment_icon} alt="" />
+          <div>
+            <p>{dashData.appointments}</p>
+            <p>Appointments</p>
+          </div>
+        </div>
+
+        <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
+          <img className='w-14' src={assets.patients_icon} alt="" />
+          <div>
+            <p>{dashData.patients}</p>
+            <p>Patients</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
